93bf9178b7fc3c26de8684afccfe79872b9e62f1
DC-Melo <melo.dachor@gmail.com>
2020-05-16 18:52:38 +0800

    add pull blog

--- pullblog.sh
+++ pullblog.sh
@@ -4,13 +4,13 @@ if [  -n "$(uname -a | grep -i ubuntu)" ]; then
     echo pull blog by ubuntu
     # :
 elif [  -n "$(uname -a | grep -i centos)" ]; then
-    echo push blog by centos
+    echo pull blog by centos
     # :
 elif [  -n "$(uname -a | grep -i darwin)" ]; then
-    echo push blog by mac
+    echo pull blog by mac
     # :
 elif [  -n "$(uname -a | grep -i android)" ]; then
-    echo push blog by android
+    echo pull blog by android
     export dbg=$HOME/blog
 else
     echo Unknown system, check $0 shell
